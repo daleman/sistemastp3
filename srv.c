cd17465c419b51d67bf66c853f1811bd16997cc5
lui <lui@lui-VPCEH15FX.(none)>
2012-11-19 12:49:29 -0300

    asd

--- srv.c
+++ srv.c
@@ -14,8 +14,8 @@ void servidor(int mi_cliente)
 	  	
     int cant_servidores = cant_ranks / 2;
  
-    int request_diferido[cant_servidores];	// un "booleano" para decir que servidores estan diferidos, es decir
-						// que pidieron el mutex para sus clientes y todavia no lo obtuvieron
+    int request_diferido[cant_servidores];	/* un "booleano" para decir que servidores estan diferidos, es decir
+						que pidieron el mutex para sus clientes y todavia no lo obtuvieron */
 	char buffer[540];
 	
 	int serv_finalizaron[cant_servidores];
@@ -34,7 +34,8 @@ void servidor(int mi_cliente)
 	
 	int el_mayor = 0;		// el mayor numero de secuencia enviado o recibido por mi servidor
 	
-	int faltantes = 1;		// la cantidad de replies que faltan de todos los servidores para obtener el mutex, lo inicializo con un numero 							//positivo asi no me da el mutex de inmediato sin pensarlo 
+	int faltantes = 1;		/* la cantidad de replies que faltan de todos los servidores para obtener el mutex,
+					lo inicializo con un numero positivo asi no me da el mutex de inmediato sin pensarlo*/ 
 	int mi_numero = 0;
 	int buf_entrante;
 
@@ -50,10 +51,11 @@ void servidor(int mi_cliente)
 	   		sprintf(buffer,"Mi cliente %d solicita acceso exclusivo", (mi_cliente-1)/2);
             debug(buffer);
             assert( hay_pedido_local == FALSE);
-            hay_pedido_local = TRUE;						// yo quiero el mutex
-            el_mayor++;		//incremento el mayor
+            hay_pedido_local = TRUE;					// yo quiero el mutex
+            el_mayor++;							//incremento el mayor
 			mi_numero = el_mayor;
-			faltantes = cant_servidores - 1 ;	//faltan todos los servidores menos yo, empiecen a mandar mensajitos... cant_ser								vidores esta actualizado con la cantidad sin incluir a los finalizados
+			faltantes = cant_servidores - 1 ;		/*faltan todos los servidores menos yo, empiecen a mandar mensajitos...
+									 cant_servidores esta actualizado con la cantidad sin incluir a los finalizados*/
 				if (faltantes == 0) {
 				debug("solo hay un servidor");
 				MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
@@ -162,7 +164,7 @@ void servidor(int mi_cliente)
 			int n = 0;
 			int serv = 0;
 			while(n < cant_servidores){
-					if( (2*n) != mi_rank ){//			!serv_finalizaron[n] si no finalizo le mando un mensaje que me voy
+					if( (2*n) != mi_rank ){		//!serv_finalizaron[n] si no finalizo le mando un mensaje que me voy
 						serv = 2*n;
 						sprintf(buffer,"mandando TAG_ME_VOY al servidor %d", n);
 						debug(buffer);
